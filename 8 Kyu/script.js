fb1d105d51d216fb4649d66a1daad2a033c36fe2
Olesia Polevoi <olesia.polevoi@gmail.com>
2022-02-06 18:27:05 -0500

    repeatStr solved

--- 8 Kyu/script.js
+++ 8 Kyu/script.js
@@ -1,3 +1,19 @@
 "use strict";
 
-// Write a function called repeatStr which repeats the given string string exactly n times.
+//NOTE String repeat. Write a function called repeatStr which repeats the given string string exactly n times.
+
+function repeatStr(n, s) {
+  let newArr = [];
+  for (let i = 0; i < n; i++) {
+    newArr.push(s);
+  }
+  return newArr.join("");
+}
+
+let test = repeatStr(6, "I");
+// "IIIIII"
+console.log(test);
+
+let test2 = repeatStr(5, "Hello");
+// "HelloHelloHelloHelloHello"
+console.log(test2);
