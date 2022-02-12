489abc7486de28a92d2003fdca2d1ed2d0da4d73
Olesia Polevoi <olesia.polevoi@gmail.com>
2022-02-11 22:30:33 -0500

    School Paperwork copy

--- 8 Kyu/script.js
+++ 8 Kyu/script.js
@@ -18,21 +18,40 @@
 // // "HelloHelloHelloHelloHello"
 // console.log(test2);
 
-// NOTE Convert number to reversed array of digits Given a random non-negative number, you have to return the digits of this number within an array in reverse order
+// // NOTE Convert number to reversed array of digits Given a random non-negative number, you have to return the digits of this number within an array in reverse order
 
-function digitize(n) {
-  let toString = "" + n;
-  let newArr = toString.split("");
-  let newArr2 = [];
-  newArr.forEach((e) => {
-    newArr2.unshift(+e);
-  });
-  return newArr2;
+// function digitize(n) {
+//   let toString = "" + n;
+//   let newArr = toString.split("");
+//   let newArr2 = [];
+//   newArr.forEach((e) => {
+//     newArr2.unshift(+e);
+//   });
+//   return newArr2;
+// }
+
+// const test = digitize(35231);
+// //[1,3,2,5,3]
+// console.log(test);
+// const test2 = digitize(0);
+// //[0]
+// console.log(test2);
+
+//NOTE Beginner Series #1 School Paperwork. Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages. Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+function paperwork(n, m) {
+  if (n < 0 || m < 0) {
+    return 0;
+  } else {
+    return m * n;
+  }
 }
-
-const test = digitize(35231);
-//[1,3,2,5,3]
+const test = paperwork(5, 5);
+// 25
 console.log(test);
-const test2 = digitize(0);
-//[0]
+const test2 = paperwork(5, -5);
+//0
 console.log(test2);
+const test3 = paperwork(-5, -5);
+// 0
+console.log(test3);
