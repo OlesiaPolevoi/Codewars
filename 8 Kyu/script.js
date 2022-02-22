a55ceb2f15276cc63817f2a13b34e98a5619e408
Olesia Polevoi <olesia.polevoi@gmail.com>
2022-02-21 23:23:05 -0500

    rock paper scissors done

--- 8 Kyu/script.js
+++ 8 Kyu/script.js
@@ -121,3 +121,54 @@
 // // even_or_odd(0);
 
 // console.log(6 % 2);
+
+// //NOTE Rock Paper Scissors
+// // Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// const rps = (p1, p2) => {
+//   if (p1 === p2) {
+//     return "Draw!";
+//   }
+//   if (p1 === "scissors" && p2 === "paper") {
+//     return "Player 1 won!";
+//   }
+//   if (p1 === "scissors" && p2 === "rock") {
+//     return "Player 2 won!";
+//   }
+//   if (p1 === "paper" && p2 === "rock") {
+//     return "Player 1 won!";
+//   }
+//   if (p1 === "paper" && p2 === "scissors") {
+//     return "Player 2 won!";
+//   }
+//   if (p1 === "rock" && p2 === "scissors") {
+//     return "Player 1 won!";
+//   }
+//   if (p1 === "rock" && p2 === "paper") {
+//     return "Player 2 won!";
+//   }
+
+//   if (p2 === "scissors" && p1 === "paper") {
+//     return "Player 2 won!";
+//   }
+//   if (p2 === "scissors" && p1 === "rock") {
+//     return "Player 1 won!";
+//   }
+//   if (p2 === "paper" && p1 === "rock") {
+//     return "Player 2 won!";
+//   }
+//   if (p2 === "paper" && p1 === "scissors") {
+//     return "Player 1 won!";
+//   }
+//   if (p2 === "rock" && p1 === "scissors") {
+//     return "Player 2 won!";
+//   }
+//   if (p2 === "rock" && p1 === "paper") {
+//     return "Player 2 won!";
+//   }
+// };
+
+// let test = rps("paper", "rock"); // Player 1 won!
+// console.log(test);
+// // rps("scissors", "rock"); // Player 2 won!
+// // rps("paper", "paper"); // Draw!
