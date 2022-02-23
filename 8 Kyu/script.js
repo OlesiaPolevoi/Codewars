495b3c0fdbeb0cd95423546caf4e9ac29958e4cd
Olesia Polevoi <olesia.polevoi@gmail.com>
2022-02-23 17:10:00 -0500

    multiplicative persistence done

--- 8 Kyu/script.js
+++ 8 Kyu/script.js
@@ -208,15 +208,23 @@
 // // The function takes a name as its only argument, and returns one of the following strings: name + " plays banjo"
 // // name + " does not play banjo"
 
-// function areYouPlayingBanjo(name) {
-//   let nameLowerCase = name.toLowerCase();
-//   if (nameLowerCase[0] === "r") {
-//     return name + " plays banjo";
-//   } else {
-//     return name + " does not play banjo";
-//   }
-// }
+// // function areYouPlayingBanjo(name) {
+// //   let nameLowerCase = name.toLowerCase();
+// //   if (nameLowerCase[0] === "r") {
+// //     return name + " plays banjo";
+// //   } else {
+// //     return name + " does not play banjo";
+// //   }
+// // }
 
+// // Using ternary operator
+// function areYouPlayingBanjo(name) {
+//   return (
+//     name +
+//     (name[0].toLowerCase() == "r" ? " plays" : " does not play") +
+//     " banjo"
+//   );
+// }
 // let test = areYouPlayingBanjo("Bravo");
 // //"bravo does not play banjo");
 // console.log(test);
