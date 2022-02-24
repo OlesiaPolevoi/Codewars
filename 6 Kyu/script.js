31289eb124af3de52c5f94c7d2e2a6017d138e02
Olesia Polevoi <olesia.polevoi@gmail.com>
2022-02-23 22:01:58 -0500

    update

--- 6 Kyu/script.js
+++ 6 Kyu/script.js
@@ -1,31 +1,30 @@
-//NOTE Persistent Bugger.
-//Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
-// For example (Input --> Output):
-// 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
-// 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
-// 4 --> 0 (because 4 is already a one-digit number)
+// //NOTE Persistent Bugger.
+// //Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+// // For example (Input --> Output):
+// // 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+// // 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+// // 4 --> 0 (because 4 is already a one-digit number)
 
-function persistence(num) {
-  let newArr = num.toString().split("");
-  let count = 0;
+// function persistence(num) {
+//   let newArr = num.toString().split("");
+//   let count = 0;
 
-  while (newArr.length > 1) {
-    let result = 1;
+//   while (newArr.length > 1) {
+//     let result = 1;
+//     for (let i = 0; i < newArr.length; i++) {
+//       result *= newArr[i];
+//     }
 
-    for (let i = 0; i < newArr.length; i++) {
-      result *= newArr[i];
-    }
+//     count++;
+//     newArr = result.toString().split("");
+//   }
 
-    count++;
-    newArr = result.toString().split("");
-  }
+//   return count;
+// }
 
-  return count;
-}
-
-// let test = persistence(4);
-// //0
-// console.log(test);
-let test1 = persistence(999);
-// //2
-console.log(test1);
+// // let test = persistence(4);
+// // //0
+// // console.log(test);
+// let test1 = persistence(999);
+// // //2
+// console.log(test1);
